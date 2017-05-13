8a2d6e031fde71bcb0147b74e6bf98213d05d6f4
Eric_Wenyi <ericwenyi1995@outlook.com>
2017-05-13 20:24:42 +0800

    provide the code for my courses

---
+++ Courses/Code/CheckCorrectness.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def CheckCorrectness(res, question_word):
+
+	counter = 0
+
+	for highlightWords in res:
+		content = highlightWords.get_text()
+		
+		if content.find(question_word) != -1:
+			counter += 1
+
+	if counter > 3:
+		return True
+	else:
+		return False
